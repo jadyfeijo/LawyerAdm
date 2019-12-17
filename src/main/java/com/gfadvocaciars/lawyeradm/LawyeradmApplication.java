a3b705a37603d16e4513fa52f0cf387a6776947b
Jady Feijó <jadyfeijo@hotmail.com>
2019-12-17 17:01:50 -0300

    inicial springboot structure

---
+++ src/main/java/com/gfadvocaciars/lawyeradm/LawyeradmApplication.java
@@ -0,0 +1,13 @@
+package com.gfadvocaciars.lawyeradm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawyeradmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawyeradmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gfadvocaciars/lawyeradm/LawyeradmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfadvocaciars.lawyeradm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawyeradmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
