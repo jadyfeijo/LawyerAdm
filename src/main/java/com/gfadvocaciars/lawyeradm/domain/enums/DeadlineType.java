66aa7f33a703d1095036709de171466e480e92d3
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2020-02-06 00:09:04 -0300

    Create daysOfDeadline attribute

--- src/main/java/com/gfadvocaciars/lawyeradm/domain/enums/DeadlineType.java
+++ src/main/java/com/gfadvocaciars/lawyeradm/domain/enums/DeadlineType.java
@@ -1,11 +1,18 @@
 package com.gfadvocaciars.lawyeradm.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum DeadlineType {
-    RÉPLICA,
-    PROVAS,
-    CONTRARRAZÕES,
-    RECURSO,
-    APELAÇÃO,
+    RÉPLICA("Réplica", 15),
+    PROVAS("Provas", 10),
+    CONTRARRAZÕES("Contrarrazões", 15),
+    RECURSO("Recurso", 10),
+    APELAÇÃO("Apelação", 15),
+    ;
 
-
+    private  String type;
+    private  Integer daysOfDeadline;
 }
