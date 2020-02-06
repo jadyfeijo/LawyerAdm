ce29d092b9b7fec787c7cae7900c9cf3273745a7
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2020-02-06 00:11:36 -0300

    Calculate business days to deadline end

--- src/main/java/com/gfadvocaciars/lawyeradm/services/DeadlineService.java
+++ src/main/java/com/gfadvocaciars/lawyeradm/services/DeadlineService.java
@@ -1,11 +1,15 @@
 package com.gfadvocaciars.lawyeradm.services;
 
 import com.gfadvocaciars.lawyeradm.domain.Deadline;
+import com.gfadvocaciars.lawyeradm.domain.enums.DeadlineStatus;
 import com.gfadvocaciars.lawyeradm.repositories.DeadlineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 @Service
 public class DeadlineService {
@@ -13,11 +17,44 @@ public class DeadlineService {
     @Autowired
     private DeadlineRepository repo;
 
-    public List<Deadline> findAll(){
+    public List<Deadline> findAll() {
         return repo.findAll();
     }
 
     public Deadline create(Deadline deadline) {
+        int daysToEnd;
+        if (deadline.getDeadline() != null) {
+            daysToEnd = deadline.getDeadline();
+        } else {
+            daysToEnd = deadline.getType().getDaysOfDeadline();
+        }
+        deadline.setFinalDate(calculateBusinessDayDeadline(deadline.getInitialDate(), daysToEnd));
+        deadline.setStatus(DeadlineStatus.ANDAMENTO);
         return repo.save(deadline);
+    }
+
+    private Date calculateBusinessDayDeadline(Date initialDate, int deadline) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(initialDate);
+        calendar.setTimeZone(TimeZone.getDefault());
+
+        for (int i = 0; i < deadline; i++) {
+
+            switch (calendar.get(Calendar.DAY_OF_WEEK)) {
+                case Calendar.FRIDAY:
+                    calendar.add(Calendar.DAY_OF_WEEK, 3);
+                    break;
+
+                case Calendar.SATURDAY:
+                    calendar.add(Calendar.DAY_OF_WEEK, 2);
+                    break;
+
+                default:
+                    calendar.add(Calendar.DAY_OF_WEEK, 1);
+            }
+        }
+
+        return calendar.getTime();
     }
 }
