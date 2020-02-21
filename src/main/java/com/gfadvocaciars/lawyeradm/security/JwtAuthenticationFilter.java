787ba9ff7b9a9d24ee734b3b571b6737c3952fe4
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2020-02-21 01:37:44 -0300

    update gitignore

---
+++ src/main/java/com/gfadvocaciars/lawyeradm/security/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.gfadvocaciars.lawyeradm;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gfadvocaciars.lawyeradm.domain.User;
+import com.gfadvocaciars.lawyeradm.domain.UserLoginInfo;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+    private JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) {
+        try {
+            UserLoginInfo user = new ObjectMapper().readValue(req.getInputStream(), UserLoginInfo.class);
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword(), new ArrayList<>());
+
+            Authentication auth = authenticationManager.authenticate(authToken);
+            return auth;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication auth) {
+
+        String username = ((User)auth.getPrincipal()).getUsername();
+        String token = jwtUtil.generateToken(username);
+        res.addHeader("Authorization","Bearer "+ token);
+    }
+
+}
---
+++ src/main/java/com/gfadvocaciars/lawyeradm/security/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.gfadvocaciars.lawyeradm;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private String secret = "secret";
+
+    private Long expiration = 600L;
+
+    public String generateToken(String username){
+
+    return JWT
+            .create()
+            .withIssuedAt(new Date())
+            .withClaim("userId", username)
+                .sign(algorithm());
+}
+
+    private Algorithm algorithm() {
+        try {
+            return Algorithm.HMAC256(secret);
+        } catch (UnsupportedEncodingException cause) {
+            throw new RuntimeException("Could not sign request", cause);
+        }
+    }
+}
