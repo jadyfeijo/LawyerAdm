77fdfc858e2cf477e2dc7594dbdc23b6cff90cd2
Jady Feijo <jadyfeijo@MacBook-Pro-de-Jady.local>
2020-02-21 01:54:51 -0300

    create login infos

--- src/main/java/com/gfadvocaciars/lawyeradm/domain/Employee.java
+++ src/main/java/com/gfadvocaciars/lawyeradm/domain/Employee.java
@@ -1,5 +1,6 @@
 package com.gfadvocaciars.lawyeradm.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gfadvocaciars.lawyeradm.domain.enums.MaritalStatus;
 import com.gfadvocaciars.lawyeradm.domain.enums.Permission;
 import org.springframework.data.annotation.Id;
@@ -7,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Employee extends NaturalPerson implements Serializable {
@@ -14,16 +16,21 @@ public class Employee extends NaturalPerson implements Serializable {
 
     @Id
     private String id;
-    private Permission permission;
+    private String email;
+    private List<Permission> permission;
+
+    private String password;
 
     public Employee() {
         super();
     }
 
-    public Employee(String name, String phone, Address address, String cpf, String rg, Date birthdate, String occupation, MaritalStatus maritalStatus, String nacionalilty, String id, Permission permission) {
+    public Employee(String name, String phone, Address address, String cpf, String rg, Date birthdate, String occupation, MaritalStatus maritalStatus, String nacionalilty, String id, String email, List<Permission> permission, String password) {
         super(name, phone, address, cpf, rg, birthdate, occupation, maritalStatus, nacionalilty);
         this.id = id;
+        this.email = email;
         this.permission = permission;
+        this.password = password;
     }
 
     public String getId() {
@@ -34,11 +41,27 @@ public class Employee extends NaturalPerson implements Serializable {
         this.id = id;
     }
 
-    public Permission getPermission() {
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Permission> getPermission() {
         return permission;
     }
 
-    public void setPermission(Permission permission) {
+    public void setPermission(List<Permission> permission) {
         this.permission = permission;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
---
+++ src/main/java/com/gfadvocaciars/lawyeradm/domain/User.java
@@ -0,0 +1,63 @@
+package com.gfadvocaciars.lawyeradm.domain;
+
+import com.gfadvocaciars.lawyeradm.domain.enums.Permission;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class User implements UserDetails {
+
+    private String id;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User() {
+    }
+
+    public User(String id, String email, String password, List<Permission> permissions) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+//        this.authorities = permissions.stream().map(x->new SimpleGrantedAuthority(x.getPermission())).collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
